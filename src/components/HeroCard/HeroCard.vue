58bb50298fddecdf7faa1de604297f738c25c585
seiwaldm <seiwald.markus@hak-stjo.salzburg.at>
2022-11-18 14:58:22 +0100

    Revert "another try on transparent log"
    This reverts commit 270288b02accca0239cce1d87dcd47f7743fbc42.

--- src/components/HeroCard/HeroCard.vue
+++ src/components/HeroCard/HeroCard.vue
@@ -4,10 +4,12 @@ const showBiography = ref(false)
 </script>
 
 <template>
+
   <div grid class="heroCard">
-    <HeroImage :image-url="hero.images.md" @show-biography="showBiography = true" />
+    <HeroImage :image-url="hero.images.md" @showBiography="showBiography = true" />
     <HeroStats :name="hero.name" :stats="hero.powerstats" :alignment="hero.biography.alignment" />
-    <HeroModal v-if="showBiography" :name="hero.name" :appearance="hero.appearance" :biography="hero.biography" :connections="hero.connections" :work="hero.work" @hide-biography="showBiography = false" />
+    <HeroModal  v-if="showBiography" :name="hero.name" :appearance="hero.appearance" :biography="hero.biography" :connections="hero.connections" :work="hero.work" @hideBiography="showBiography = false"
+  />
   </div>
 </template>
 
